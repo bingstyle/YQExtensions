e8257c4324ed098be9813c23608aa330fd93009c
WeiXinbing <183292352@qq.com>
2019-07-17 16:53:54 +0800

    更新VC扩展

---
+++ Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewControllerExtension.swift
+//  Example
+//
+//  Created by WeiXinbing on 2019/7/17.
+//  Copyright © 2019 bing. All rights reserved.
+//
+
+import UIKit
+
+public extension UIViewController {
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        //handle the segue
+    }
+}
+
--- YQExtensions.podspec
+++ YQExtensions.podspec
@@ -2,7 +2,7 @@
 
 Pod::Spec.new do |s|
 s.name         = "YQExtensions"
-s.version      = "0.3.0"
+s.version      = "0.3.1"
 s.ios.deployment_target = '10.0'
 s.summary      = "some extensions"
 s.homepage     = "https://github.com/weixinbing/YQTools"
